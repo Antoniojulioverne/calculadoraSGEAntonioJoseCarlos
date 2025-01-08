3c59aa4fa725debd299ecfb1e8d7c6d37af15541
Antoniojulioverne <147029624+Antoniojulioverne@users.noreply.github.com>
2025-01-08 13:42:20 +0100

    Main

---
+++ calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,20 @@
+package calculadora;
+
+public class Calculadora {
+
+	private int numero;
+	private int numeroAnt;
+	
+	public Calculadora() {
+		this.numero=0;
+		this.numeroAnt=0;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculadora c = new Calculadora();
+	}
+
+}
